c625c1364f6636dc4305ef7473f3f9e6bb217724
li-shuai <506507881@qq.com>
2025-07-29 01:42:03 +0800

    fix：button测试用例

--- packages/components/Button/Button.test.tsx
+++ packages/components/Button/Button.test.tsx
@@ -197,59 +197,58 @@ describe('Button.vue', () => {
     expect(iconElement.exists()).toBeTruthy()
     expect(iconElement.attributes('icon')).toBe('arrow-up')
   })
+})
+describe('ButtonGroup.vue', () => {
+  test("basic button group", async () => {
+    const wrapper = mount(() => (
+      <ButtonGroup>
+        <Button>button 1</Button>
+        <Button>button 2</Button>
+      </ButtonGroup>
+    ));
 
-  describe('ButtonGroup.vue', () => {
-    test("basic button group", async () => {
+    expect(wrapper.classes()).toContain("xc-button-group");
+  });
+
+  test('button group size', () => {
+    const sizes = ['large', 'default', 'small']
+    sizes.forEach((size) => {
       const wrapper = mount(() => (
-        <ButtonGroup>
-          <Button>button 1</Button>
-          <Button>button 2</Button>
-        </ButtonGroup>
-      ));
-
-      expect(wrapper.classes()).toContain("xc-button-group");
-    });
-
-    test('button group size', () => {
-      const sizes = ['large', 'default', 'small']
-      sizes.forEach((size) => {
-        const wrapper = mount(() => (
-          <ButtonGroup size={size as any}>
-            <Button>button 1</Button>
-            <Button>button 2</Button>
-          </ButtonGroup>
-        ))
-
-        const buttonWrapper = wrapper.findComponent(Button)
-        expect(buttonWrapper.classes()).toContain(`xc-button--${size}`)
-      })
-    })
-
-    test('button group type', () => {
-      const types = ['primary', 'success', 'warning', 'danger', 'info']
-      types.forEach((type) => {
-        const wrapper = mount(() => (
-          <ButtonGroup type={type as any}>
-            <Button>button 1</Button>
-            <Button>button 2</Button>
-          </ButtonGroup>
-        ))
-
-        const buttonWrapper = wrapper.findComponent(Button)
-        expect(buttonWrapper.classes()).toContain(`xc-button--${type}`)
-      })
-    })
-
-    test('button group disabled', () => {
-      const wrapper = mount(() => (
-        <ButtonGroup disabled>
+        <ButtonGroup size={size as any}>
           <Button>button 1</Button>
           <Button>button 2</Button>
         </ButtonGroup>
       ))
 
       const buttonWrapper = wrapper.findComponent(Button)
-      expect(buttonWrapper.classes()).toContain(`is-disabled`)
+      expect(buttonWrapper.classes()).toContain(`xc-button--${size}`)
     })
+  })
+
+  test('button group type', () => {
+    const types = ['primary', 'success', 'warning', 'danger', 'info']
+    types.forEach((type) => {
+      const wrapper = mount(() => (
+        <ButtonGroup type={type as any}>
+          <Button>button 1</Button>
+          <Button>button 2</Button>
+        </ButtonGroup>
+      ))
+
+      const buttonWrapper = wrapper.findComponent(Button)
+      expect(buttonWrapper.classes()).toContain(`xc-button--${type}`)
+    })
+  })
+
+  test('button group disabled', () => {
+    const wrapper = mount(() => (
+      <ButtonGroup disabled>
+        <Button>button 1</Button>
+        <Button>button 2</Button>
+      </ButtonGroup>
+    ))
+
+    const buttonWrapper = wrapper.findComponent(Button)
+    expect(buttonWrapper.classes()).toContain(`is-disabled`)
   })
 })
