1a77a799b789ad658dbb1ee55d853ed8c89890b7
li-shuai <506507881@qq.com>
2025-08-27 01:40:04 +0800

    feat:完善button样式

--- packages/docs/demo/button/Disabled.vue
+++ packages/docs/demo/button/Disabled.vue
@@ -1,18 +1,46 @@
 <template>
   <p>
     <ns-button>Default</ns-button>
-    <ns-button type="primary">Primary</ns-button>
-    <ns-button type="success">Success</ns-button>
-    <ns-button type="info">Info</ns-button>
-    <ns-button type="warning">Warning</ns-button>
-    <ns-button type="danger">Danger</ns-button>
+    <ns-button status="primary">Primary</ns-button>
+    <ns-button status="success">Success</ns-button>
+    <!-- <ns-button type="info">Info</ns-button> -->
+    <ns-button status="warning">Warning</ns-button>
+    <ns-button status="danger">Danger</ns-button>
   </p>
   <p>
     <ns-button disabled>Default</ns-button>
-    <ns-button type="primary" disabled>Primary</ns-button>
-    <ns-button type="success" disabled>Success</ns-button>
-    <ns-button type="info" disabled>Info</ns-button>
-    <ns-button type="warning" disabled>Warning</ns-button>
-    <ns-button type="danger" disabled>Danger</ns-button>
+    <ns-button status="primary" disabled>Primary</ns-button>
+    <ns-button status="success" disabled>Success</ns-button>
+    <!-- <ns-button type="info" disabled>Info</ns-button> -->
+    <ns-button status="warning" disabled>Warning</ns-button>
+    <ns-button status="danger" disabled>Danger</ns-button>
+  </p>
+  <p>
+    <ns-button type="secondary">Default</ns-button>
+    <ns-button type="secondary" status="primary">Primary</ns-button>
+    <ns-button type="secondary" status="success">Success</ns-button>
+    <ns-button type="secondary" status="warning">Warning</ns-button>
+    <ns-button type="secondary" status="danger">Danger</ns-button>
+  </p>
+  <p>
+    <ns-button type="secondary" disabled>Disabled</ns-button>
+    <ns-button type="secondary" status="primary" disabled>Primary</ns-button>
+    <ns-button type="secondary" status="success" disabled>Success</ns-button>
+    <ns-button type="secondary" status="warning" disabled>Warning</ns-button>
+    <ns-button type="secondary" status="danger" disabled>Danger</ns-button>
+  </p>
+  <p>
+    <ns-button type="outline">Default</ns-button>
+    <ns-button type="outline" status="primary">Primary</ns-button>
+    <ns-button type="outline" status="success">Success</ns-button>
+    <ns-button type="outline" status="warning">Warning</ns-button>
+    <ns-button type="outline" status="danger">Danger</ns-button>
+  </p>
+  <p>
+    <ns-button type="outline" disabled>Disabled</ns-button>
+    <ns-button type="outline" status="primary" disabled>Primary</ns-button>
+    <ns-button type="outline" status="success" disabled>Success</ns-button>
+    <ns-button type="outline" status="warning" disabled>Warning</ns-button>
+    <ns-button type="outline" status="danger" disabled>Danger</ns-button>
   </p>
 </template>
